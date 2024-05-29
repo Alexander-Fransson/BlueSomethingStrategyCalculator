126ff02cbc9ce3434e9875a2e64f9be2af9a11d7
Alexander-Fransson <festusOp7@gmail.com>
2024-05-29 22:08:39 +0200

    [finish] it was fun for a while but I dont have time, maybe revisit this at some point, at least I got to practice my python

---
+++ decisions_modules/finance.py
@@ -0,0 +1,42 @@
+class BankInterestNegotiations:
+    def __init__(
+        self,
+        promised_circularity
+    ):
+        self.circularity = promised_circularity
+
+        #ICA Bancas interests dependant on circularity
+        self.index_at_zero = 1.2136
+        self.index_at_hundered = 0.9136
+        self.interest_at_zero = .0607
+        self.interest_at_hundered = .0457
+
+        #Past interest rates on loans I belive
+        self.real_estate_loan_margin = .04 
+        self.machine_loan_margin = .05
+        self.overdraft_loan_margin = .06 #emergency loan?
+
+        self.current_emergency_loan_rate = .28
+
+    def vendor_lease(durability:float,duration:str, demand, basic_price):
+        amount = duration * demand * basic_price
+
+    # assumes linear relationship based on circularity
+    def bank_interest_rate_based_on_circularity(self, margin):
+
+        decreased_index_per_percent_circularity = (
+            self.index_at_zero - self.index_at_hundered
+        )/100
+        
+        decreased_interest_per_percent_circularity = (
+            self.interest_at_zero - self.interest_at_hundered
+        )/100
+
+        current_index = self.index_at_zero - decreased_index_per_percent_circularity
+        current_base_interest = self.interest_at_zero - decreased_interest_per_percent_circularity
+
+        return current_index * (current_base_interest + margin)
+
+# not matching income statement?
+starting_cash_position = 20800
+starting_emergency_loans = 4896733
